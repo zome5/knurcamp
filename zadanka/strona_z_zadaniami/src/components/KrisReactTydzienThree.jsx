5581732dae8904a280c7a539d09b8f38c8853946
postmahone <oliwer.kirkiewicz@gmail.com>
2024-02-24 20:17:12 +0100

    stworzenie komponentu SygnalizacjaV3

--- zadanka/strona_z_zadaniami/src/components/KrisReactTydzienThree.jsx
+++ zadanka/strona_z_zadaniami/src/components/KrisReactTydzienThree.jsx
@@ -1,9 +1,11 @@
 import { SygnalizacjaV2 } from "./zadanka_od_Krisa/SygnalizacjaV2";
+import { SygnalizacjaV3 } from "./zadanka_od_Krisa/SygnalizacjaV3";
 
 const KrisReactTydzienThree = () => {
   return (
     <>
       <SygnalizacjaV2></SygnalizacjaV2>
+      <SygnalizacjaV3></SygnalizacjaV3>
     </>
   );
 };
---
+++ zadanka/strona_z_zadaniami/src/components/zadanka_od_Krisa/SygnalizacjaV3.jsx
@@ -0,0 +1,7 @@
+export const SygnalizacjaV3 = () => {
+  return (
+    <>
+      <h1>Sygnalizacja V3</h1>
+    </>
+  );
+};
