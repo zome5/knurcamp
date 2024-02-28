f58673b2819b37aab02e78bda539938a3b5f34ee
postmahone <oliwer.kirkiewicz@gmail.com>
2024-02-28 13:11:52 +0100

    Lista Zakupów Działająca Wersja 1

--- zadanka/strona_z_zadaniami/src/components/zadanka_od_Krisa/Notatki.jsx
+++ zadanka/strona_z_zadaniami/src/components/zadanka_od_Krisa/Notatki.jsx
@@ -1,4 +1,3 @@
-// import { NotatkiForm } from "../small_components/NotatkiForm";
 import { useState } from "react";
 
 export const Notatki = () => {
@@ -6,18 +5,23 @@ export const Notatki = () => {
   const [pracaList, setPracaList] = useState([]);
   const [studiaList, setStudiaList] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [currentSection, setCurrentSection] = useState(zakupyList);
-  const [currentSetSection, setCurrentSetSection] = useState(setZakupyList);
+  const [currentSection, setCurrentSection] = useState("Lista Zakupów");
+  const handleNavClick = (nameOfThisElement) => {
+    setCurrentSection(nameOfThisElement);
+  };
   const handleNavStyles = (nameOfThisElement) => {
     if (currentSection === nameOfThisElement) {
       return {
         border: "1px solid #4191c0",
       };
     }
+    return {};
   };
-  const handleNavClick = (state, setState) => {
-    setCurrentSection(state);
-    setCurrentSetSection(setState);
+
+  const renderList = (list) => {
+    return list.map((listElement, i) => (
+      <p key={`${listElement}-${i}`}>{listElement}</p>
+    ));
   };
 
   return (
@@ -26,19 +30,28 @@ export const Notatki = () => {
       <div className="notatki-wrapper">
         <div className="notatki-nav">
           <h3
-            onClick={() => handleNavClick(zakupyList, setZakupyList)}
+            onClick={() => {
+              handleNavClick("Lista Zakupów");
+              setInputValue("");
+            }}
             style={handleNavStyles("Lista Zakupów")}
           >
             Lista Zakupów
           </h3>
           <h3
-            onClick={() => handleNavClick(pracaList, setPracaList)}
+            onClick={() => {
+              handleNavClick("Praca");
+              setInputValue("");
+            }}
             style={handleNavStyles("Praca")}
           >
             Praca
           </h3>
           <h3
-            onClick={() => handleNavClick(studiaList, setStudiaList)}
+            onClick={() => {
+              handleNavClick("Studia");
+              setInputValue("");
+            }}
             style={handleNavStyles("Studia")}
           >
             Studia
@@ -54,10 +67,18 @@ export const Notatki = () => {
             />
             <button
               type="button"
-              onClick={(e) => {
-                currentSetSection((prev) => {
-                  return [...prev, inputValue];
-                });
+              onClick={() => {
+                switch (currentSection) {
+                  case "Lista Zakupów":
+                    setZakupyList((prev) => [...prev, inputValue]);
+                    break;
+                  case "Praca":
+                    setPracaList((prev) => [...prev, inputValue]);
+                    break;
+                  case "Studia":
+                    setStudiaList((prev) => [...prev, inputValue]);
+                    break;
+                }
                 setInputValue("");
               }}
             >
@@ -65,9 +86,11 @@ export const Notatki = () => {
             </button>
           </form>
           <section>
-            {currentSection.map((listElement, i) => (
-              <p key={`${listElement}-${i}`}>{listElement}</p>
-            ))}
+            {currentSection === "Lista Zakupów"
+              ? renderList(zakupyList)
+              : currentSection === "Praca"
+              ? renderList(pracaList)
+              : renderList(studiaList)}
           </section>
         </div>
       </div>
